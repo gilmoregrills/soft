3fba708815722f6b55fdbe064260d5365bb7085c
Your Name <you@example.com>
2025-10-29 15:30:24 +0000

    lualine

--- lua/soft/lualine.lua
+++ lua/soft/lualine.lua
@@ -4,31 +4,54 @@ local normal = {
 	a = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 	b = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
 	c = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	d = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	e = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	f = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 }
 
 local command = {
 	a = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 	b = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	c = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	d = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	e = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	f = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 }
 
 local visual = {
 	a = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 	b = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	c = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	d = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	e = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	f = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 }
 
 local inactive = {
 	a = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 	b = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	c = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	d = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	e = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	f = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 }
 
 local replace = {
 	a = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 	b = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	c = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	d = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	e = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	f = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 }
 
 local insert = {
 	a = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 	b = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	c = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	d = { fg = colors.purpleDefault, bg = colors.purpleLight },
+	e = { fg = colors.purpleDarkest, bg = colors.purpleDarker },
+	f = { fg = colors.black, bg = colors.purpleDarkest, gui = "bold" },
 }
 
 return {
